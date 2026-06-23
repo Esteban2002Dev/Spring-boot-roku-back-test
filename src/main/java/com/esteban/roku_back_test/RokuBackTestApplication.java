1359561e14aea3de8b5594bfaea6a043c089e2f0
Esteban2002Dev <esteban.software.dev@gmail.com>
2026-06-22 18:46:36 -0600

    first commit

---
+++ src/main/java/com/esteban/roku_back_test/RokuBackTestApplication.java
@@ -0,0 +1,13 @@
+package com.esteban.roku_back_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokuBackTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RokuBackTestApplication.class, args);
+	}
+
+}
